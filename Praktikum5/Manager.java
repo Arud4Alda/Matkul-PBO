83e11bc981573ba58140823d0c464665de18c903
aruda <aldpj27@gmail.com>
2025-09-18 16:57:07 +0700

    menyelesaikan task 3.1

---
+++ Praktikum5/Manager.java
@@ -0,0 +1,34 @@
+import java.util.GregorianCalendar;
+import java.util.Calendar;
+public class Manager extends Employee {
+    private String secretaryName;
+
+    public Manager(String n, double s, int d, int m, int y) {
+        super(n, s, d, m, y);
+        secretaryName = "";
+    }
+
+    public void raiseSalary(double byPercent) {
+        // add 1/2% bonus for every year of service
+        GregorianCalendar todaysDate = new GregorianCalendar();
+        int currentYear = todaysDate.get(Calendar.YEAR);
+        double bonus = 0.5 * (currentYear - hireYear());
+        super.raiseSalary(byPercent + bonus);
+    }
+
+    public String getSecretaryName() {
+        return secretaryName;
+    }
+
+    @Override
+    public int compare(Sortable b) {
+        Manager mb = (Manager) b;
+        if (super.getSalary() < mb.getSalary()) {
+            return -1;
+        }
+        if (super.getSalary() > mb.getSalary()) {
+            return +1;
+        }
+        return 0;
+    }
+}
